03f549fbeae455a6281a232dbcd3dc9194b374a5
Mayank-Prakash-Sharma18 <mragank.shandilya@gmail.com>
2021-01-23 18:06:50 +0530

    13 herbs article added, plus some CSS settings

--- assets/js/script.js
+++ assets/js/script.js
@@ -123,3 +123,13 @@ $(window).on('load', function () {
 
 
 })(jQuery);
+
+
+// Disable right click (https://www.w3docs.com/snippets/javascript/how-to-disable-text-selection-copy-cut-paste-and-right-click-on-a-web-page.html)
+
+src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.0/jquery.min.js"
+$(document).ready(function() {
+  $("body").on("contextmenu", function(e) {
+      return false;
+    });
+});
